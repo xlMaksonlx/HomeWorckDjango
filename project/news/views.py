65f723cbe44cc4c2875f7abe9cdf564e50b9c46c
xlMaksonlx <gwynbleidd_1989@list.ru>
2024-08-15 14:15:43 +0500

    HomeWork D7

--- project/news/views.py
+++ project/news/views.py
@@ -1,18 +1,28 @@
 from datetime import datetime
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .filters import PostFilter
 from .models import Post
 
 
 class PostsListView(ListView):
     model = Post
-    ordering = 'date_in'
+    ordering = '-date_in'
     template_name = 'posts.html'
     context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
         return context
+
+
+
 
 
 class PostDetail(DetailView):
